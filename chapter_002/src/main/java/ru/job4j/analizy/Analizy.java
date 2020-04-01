5e79737d901b2798daa306c8d438477ad817bce3
mib1 <mib1@inbox.ru>
2020-04-01 12:30:53 +0300

    Analizy

--- chapter_002/src/main/java/ru/job4j/analizy/Analizy.java
+++ chapter_002/src/main/java/ru/job4j/analizy/Analizy.java
@@ -1,44 +1,59 @@
 package ru.job4j.analizy;
 
+import java.io.BufferedReader;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Analizy {
-    public void unavailable(String source, String target) {
+    List<String> listin = new ArrayList<>();
+    List<String> listout = new ArrayList<>();
+    boolean unavailable = false;
 
+    public void unavailable(String source, String target) {
+        System.out.println();
+
+        readfile(source);
+
+        String begin = "";
+        String end;
+        for (String str : listin) {
+            String[] l = str.split(" ");
+            if (l[0].equals("500") || l[0].equals("400")) {
+                if (!unavailable) {
+                    begin = l[1];
+                    unavailable = true;
+                }
+            } else if (unavailable) {
+                end = l[1];
+                unavailable = false;
+                listout.add(begin + ";" + end);
+            }
+        }
+        listout.forEach(System.out::println);
+
+        savefile(target);
     }
 
     public static void main(String[] args) {
-        try (PrintWriter out = new PrintWriter(new FileOutputStream("unavailable.csv"))) {
-            out.println("15:01:30;15:02:32");
-            out.println("15:10:30;23:12:32");
+        new Analizy().unavailable("chapter_002/data/server.log", "chapter_002/data/unavailable.csv");
+    }
+
+    void readfile(String path) {
+        try (BufferedReader read = new BufferedReader(new FileReader(path))) {
+            read.lines().forEach(n -> this.listin.add(n));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    void savefile(String target) {
+        try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
+            listout.stream().forEach(n -> out.println(n));
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 }
-
-
-
-     // 1. Реализуйте метод unavailable.
-     // source - имя файла лога
-     // target - имя файла после анализа.
-     //
-     // 2. Метод anavailable должен находить диапазоны, когда сервер не работал.
-     // Сервер не работал. если status = 400 или 500.
-     // Диапазон считается последовательность статусов 400 и 500
-     // Например:
-     // 200 10:56:01
-     // 500 10:57:01
-     // 400 10:58:01
-     // 200 10:59:01
-     // 500 11:01:02
-     // 200 11:02:02
-     // тут два периода - 10:57:01 до 10:59:01 и 11:01:02 до 11:02:02
-     // Начальное время - это время когда статус 400 или 500. конечное время это когда статус меняется с 400 или 500 на 200 300.
-     //
-     // 3. Результат анализа нужно записать в файл target.
-     // Формат файла
-     // начала периода;конец периода;
-     //
-     // 4. Записать тесты.
---
+++ chapter_002/src/test/java/ru/job4j/analizy/AnalizyTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.analizy;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class AnalizyTest {
+    List<String> expected = List.of(
+            new String("10:58:01;11:02:02"),
+            new String("11:04:01;11:06:01"),
+            new String("11:07:01;11:08:01"));
+
+    @Test
+    public void testingUnavailable() {
+        Analizy a = new Analizy();
+        a.unavailable("./data/server.log", "./data/unavailable.csv");
+        assertThat(expected, is(a.listout));
+    }
+
+    @Test
+    public void compareWithSavedFile() {
+        Analizy a = new Analizy();
+        a.unavailable("./data/server.log", "./data/unavailable.csv");
+        a.listin.clear();
+        a.readfile("./data/unavailable.csv");
+        assertThat(expected, is(a.listin));
+    }
+}
