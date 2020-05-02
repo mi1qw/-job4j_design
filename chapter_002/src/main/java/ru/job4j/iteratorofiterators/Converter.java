d4b08d31e68af9ef15d0ddbe5bb3afbf3e539edc
mib1 <mib1@inbox.ru>
2020-05-03 00:56:08 +0300

    Iterator_Of_Iterators simplest

--- chapter_002/src/main/java/ru/job4j/iteratorofiterators/Converter.java
+++ chapter_002/src/main/java/ru/job4j/iteratorofiterators/Converter.java
@@ -4,28 +4,16 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 class Converter {
-    //Iterator<Iterator<Integer>> it;
-
     Iterator<Integer> convert(final Iterator<Iterator<Integer>> it) {
         return new Iterator<Integer>() {
-            private Iterator<Integer> iterator;
-
-            {
-                this.iterator = it.next();
-            }
+            private Iterator<Integer> iterator = it.next();
 
             @Override
             public boolean hasNext() {
-                boolean res = false;
-                if (iterator.hasNext()) {
-                    res = true;
-                } else {
-                    while (it.hasNext() && !res) {
-                        iterator = it.next();
-                        res = iterator.hasNext();
-                    }
+                while (!iterator.hasNext() && it.hasNext()) {
+                    iterator = it.next();
                 }
-                return res;
+                return iterator.hasNext();
             }
 
             @Override
--- chapter_002/src/main/java/ru/job4j/iteratorofiterators/IteratorOfIterators.java
+++ chapter_002/src/main/java/ru/job4j/iteratorofiterators/IteratorOfIterators.java
@@ -9,18 +9,16 @@ public class IteratorOfIterators {
     }
 
     final void go() {
-        System.out.println("re");
-        System.out.println("Iterator");
-
         Iterator<Integer> it1 = Arrays.asList(1, 2, 3).iterator();
         Iterator<Integer> it2 = Arrays.asList(4, 5, 6).iterator();
         Iterator<Integer> it3 = Arrays.asList(7, 8, 9).iterator();
         Iterator<Iterator<Integer>> its = Arrays.asList(it1, it2, it3).iterator();
-        Converter iteratorOfIterators = new Converter();
-        Iterator<Integer> it = iteratorOfIterators.convert(its);
 
-        //it.hasNext()
+        Iterator<Integer> it = new Converter().convert(its);
 
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
     }
 }
 
