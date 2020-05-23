78558928bd98f733f8e9a167a31fe4e6e380a9d0
mib1 <mib1@inbox.ru>
2020-05-23 10:32:07 +0300

    one -> two

--- chapter_002/src/test/java/ru/job4j/user/UserTest.java
+++ chapter_002/src/test/java/ru/job4j/user/UserTest.java
@@ -19,13 +19,20 @@ public class UserTest {
         User user2 = new User("Ann",
                 1,
                 new GregorianCalendar(1995, Calendar.JANUARY, 25));
+        User user3 = new User("Ann1",
+                1,
+                new GregorianCalendar(1996, Calendar.FEBRUARY, 26));
 
-        Map<User, Object> map = new HashMap<>();
-        map.put(user1, this);
-        map.put(user2, this);
+        Map<User, String> map = new HashMap<>();
+        map.put(user1, "one");
+        map.put(user2, "two");
         System.out.println(map);
         System.out.println("user1 == user2 ? - " + (user1 == user2));
         System.out.println("user1 equals user2 ? - " + (user1.equals(user2)));
-        assertThat(user1.equals(user2), is(true));
+        System.out.println("user1 equals user2 ? - " + (user1.equals(user3)));
+        System.out.println("user1 equals user2 ? - " + (user1.equals("user3")));
+        System.out.println("user1 equals user2 ? - " + (user1.equals(user1)));
+
+        assertThat(map.get(user2), is("two"));
     }
 }
