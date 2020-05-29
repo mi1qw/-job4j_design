71682d05c110ad335f33c76521c5b03bc4187bca
mib1 <mib1@inbox.ru>
2020-05-29 13:37:58 +0300

    SimpleMap

--- chapter_002/src/test/java/ru/job4j/simplemap/SimpleMapTest.java
+++ chapter_002/src/test/java/ru/job4j/simplemap/SimpleMapTest.java
@@ -18,33 +18,40 @@ public class SimpleMapTest<T> {
     @Before
     public void setUp() {
         map = new SimpleMap<>();
-        map.put(1, "AB");
-        map.put(2, "ab");
-        map.put(3, "abc");
+        map.insert(1, "AB");
+        map.insert(2, "ab");
+        map.insert(3, "abc");
         itMap = map.iterator();
     }
 
     @Test
-    public void tableSizeFor() {
-        assertThat(SimpleMap.tableSizeFor(3), is(4));
-        assertThat(SimpleMap.tableSizeFor(5), is(8));
-    }
-
-    @Test
     public void whenDeleteShouldChangeSize() {
+        assertThat(map.size(), is(3));
         assertTrue(map.delete(1));
         assertThat(map.size(), is(2));
     }
 
     @Test
-    public void whenDeleteAnotherElement() {
+    public void whenDeleteAnotherElementThenNothingChange() {
         assertFalse(map.delete(4));
         assertThat(map.size(), is(3));
     }
 
+    @Test
+    public void whenDeleteFromEmptyTable() {
+        SimpleMap<Integer, Integer> b = new SimpleMap<>();
+        assertFalse(b.delete(1));
+    }
+
     @Test(expected = ConcurrentModificationException.class)
-    public void whenCorruptedIt() {
-        map.put(10, "10");
+    public void whenCorruptedItByDelete() {
+        map.delete(1);
+        itMap.next();
+    }
+
+    @Test(expected = ConcurrentModificationException.class)
+    public void whenCorruptedItByPut() {
+        map.insert(10, "10");
         itMap.next();
     }
 
@@ -67,10 +74,18 @@ public class SimpleMapTest<T> {
         assertThat(itMap.next().getValue(), is("abc"));
     }
 
+    @Test(expected = NoSuchElementException.class)
+    public void whenNextMoreThanArrayForTesr() {
+        itMap.next();
+        itMap.next();
+        itMap.next();
+        itMap.next();
+    }
+
     @Test
     public void createMap() {
-        SimpleMap<Integer, Integer> a = new SimpleMap<>(1, 0.75f);
-        a.put(1, 1);
+        SimpleMap<Integer, Integer> a = new SimpleMap<>(2, 0.75f);
+        a.insert(1, 1);
         assertThat(a.size(), is(1));
     }
 
@@ -85,41 +100,29 @@ public class SimpleMapTest<T> {
     }
 
     @Test
+    public void whenCreateMapWithTwoParametrs() {
+        SimpleMap<Integer, Integer> aa = new SimpleMap<>(1, 0.75f);
+        assertNull(aa.get(1));
+    }
+
+    @Test
     public void whenPutInBusyNodeThenNull() {
-        //assertNull(map.put(1, "q"));
-        assertFalse(map.put(1, "q"));
+        assertFalse(map.insert(1, "q"));
     }
 
     @Test
     public void whenMapShouldGrow() {
         for (int n = 4; n != 8; ++n) {
-            map.put(n, "a");
+            map.insert(n, "a");
         }
         assertThat(map.size(), is(7));
     }
 
     @Test
     public void anotherTests() {
-        map.put(null, "1");
-        map.get(null);
-        assertTrue(true);
+        SimpleMap<Integer, Integer> bb = new SimpleMap<>(1, 0.75f);
+        assertNull(bb.get(null));
+        bb.insert(null, 1);
+        assertFalse(map.insert(1, "aqa"));
     }
-
-    //@Test
-    //public void whenBlankString() {
-    //    //Map<String, String> list = new HashMap<>();
-    //    //list.put("name", "Petr");
-    //    //String string = "";
-    //    //SimpleMap<Integer, Integer> a = new SimpleMap<>(1, 0.75f);
-    //    //SimpleMap a = Mockito.spy(SimpleMap.class);
-    //    SimpleMap a = Mockito.spy(new SimpleMap(1, 0.75f));
-    //    for (int n = 0; n != 20; ++n) {
-    //        //noinspection unchecked
-    //        a.put(n, "mock");
-    //    }
-    //
-    //    //a..simpleGenerator(string, list);
-    //    //
-    //    //Mockito.verify(simple).blankString();
-    //}
 }
