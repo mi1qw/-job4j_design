799140d62a6bb51797f74d49ec8f99808eaf2ff5
mib1 <mib1@inbox.ru>
2020-06-02 00:55:16 +0300

    SimpleTree

---
+++ chapter_002/src/main/java/ru/job4j/tree/Tree.java
@@ -0,0 +1,44 @@
+package ru.job4j.tree;
+
+import java.util.LinkedList;
+import java.util.Optional;
+import java.util.Queue;
+
+class Tree<E> implements SimpleTree<E> {
+    private final Node<E> root;
+
+    Tree(final E root) {
+        this.root = new Node<>(root);
+    }
+
+    @Override
+    public boolean add(final E parent, final E child) {
+        Optional<Node<E>> parentNode = findBy(parent);
+        Optional<Node<E>> childNode = findBy(child);
+        if (childNode.isEmpty() && parentNode.isPresent()) {
+            Node<E> node = parentNode.get();
+            node.getChildren().add(new Node<>(child));
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Optional<Node<E>> findBy(final E value) {
+        Optional<Node<E>> rsl = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (el.getValue().equals(value)) {
+                rsl = Optional.of(el);
+                break;
+            }
+            data.addAll(el.getChildren());
+        }
+        return rsl;
+    }
+}
+
+
+
