7a873a1222f6aacff74ab9ce97eeb0367af31ed7
mib1 <mib1@inbox.ru>
2020-06-02 17:39:23 +0300

    SimpleTree + isBinary lambda

--- chapter_002/src/main/java/ru/job4j/tree/Tree.java
+++ chapter_002/src/main/java/ru/job4j/tree/Tree.java
@@ -3,6 +3,7 @@ package ru.job4j.tree;
 import java.util.LinkedList;
 import java.util.Optional;
 import java.util.Queue;
+import java.util.function.Predicate;
 
 class Tree<E> implements SimpleTree<E> {
     private final Node<E> root;
@@ -25,32 +26,25 @@ class Tree<E> implements SimpleTree<E> {
 
     @Override
     public Optional<Node<E>> findBy(final E value) {
-        Optional<Node<E>> rsl = Optional.empty();
-        Queue<Node<E>> data = new LinkedList<>();
-        data.offer(this.root);
-        while (!data.isEmpty()) {
-            Node<E> el = data.poll();
-            if (el.getValue().equals(value)) {
-                rsl = Optional.of(el);
-                break;
-            }
-            data.addAll(el.getChildren());
-        }
-        return rsl;
+        return findNode(n -> n.getValue().equals(value));
     }
 
     @Override
     public boolean isBinary() {
-        Queue<Node<E>> tree = new LinkedList<>();
-        tree.offer(this.root);
-        while (!tree.isEmpty()) {
-            Node<E> pElem = tree.poll();
-            if (pElem.getChildren().size() > 2) {
-                return false;
+        return findNode(n -> n.getChildren().size() > 2).isEmpty();
+    }
+
+    private Optional<Node<E>> findNode(final Predicate<Node<E>> p) {
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (p.test(el)) {
+                return Optional.of(el);
             }
-            tree.addAll(pElem.getChildren());
+            data.addAll(el.getChildren());
         }
-        return true;
+        return Optional.empty();
     }
 }
 
