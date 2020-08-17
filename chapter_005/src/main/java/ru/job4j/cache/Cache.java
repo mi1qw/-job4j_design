d772e81f84a02c7764a2f7d611dca5823b420851
mib1 <mib1@inbox.ru>
2020-08-17 12:19:59 +0300

    cache test

--- chapter_005/src/main/java/ru/job4j/cache/Cache.java
+++ chapter_005/src/main/java/ru/job4j/cache/Cache.java
@@ -12,15 +12,15 @@ import java.util.List;
 import java.util.Map;
 
 public abstract class Cache<E, T> implements CacheSoftReference<E, T> {
-    protected Map<E, SoftReference<T>> cache = new HashMap<>();
+    private Map<E, SoftReference<T>> cacheT = new HashMap<>();
 
     /**
      * Gets cache.
      *
      * @return the cache
      */
-    public Map<E, SoftReference<T>> getCache() {
-        return cache;
+    public Map<E, SoftReference<T>> getCacheT() {
+        return cacheT;
     }
 
     /**
@@ -29,43 +29,24 @@ public abstract class Cache<E, T> implements CacheSoftReference<E, T> {
      * @param key key
      * @return T
      */
+    @SuppressWarnings("CheckStyle")
     @Override
     public T get(final E key) {
-        SoftReference<T> softRef = cache.get(key);
-        if (softRef == null || softRef.get() == null) {
+        SoftReference<T> softRef = cacheT.get(key);
+        T ob;
+        if (softRef == null || (ob = softRef.get()) == null) {
             System.out.println(String.format("Not exist %s = %s", key,
                     softRef == null ? "new" : "null"));
             return put(key);
         }
-        return softRef.get();
+        return ob;
     }
 }
 
 
 class TextCache extends Cache<String, List<String>> implements TxtCache {
     public static final Logger LOG = LoggerFactory.getLogger(TextCache.class);
-    private Path directory = Path.of("chapter_005", "src", "main", "resources");
-
-    public static void main(final String[] args) {
-        new TextCache().go();
-    }
-
-    public void go() {
-        int n = 10;
-        while (n-- > 0) {
-            getTxt("Names.txt");
-            getTxt("Address.txt");
-            getTxt("log4j2.xml");
-            getTxt("big.txt");
-            getTxt("big2.txt");
-            get("Address.txt");
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+    private Path directory = Path.of("src", "main", "resources").toAbsolutePath();
 
     @Override
     public List<String> getTxt(final String file) {
@@ -86,7 +67,7 @@ class TextCache extends Cache<String, List<String>> implements TxtCache {
         } catch (IOException e) {
             LOG.error(e.getMessage(), e);
         }
-        super.cache.put(key, new SoftReference<>(list));
+        super.getCacheT().put(key, new SoftReference<>(list));
         return list;
     }
 }
--- chapter_005/src/test/java/ru/job4j/cache/TextCacheTest.java
+++ chapter_005/src/test/java/ru/job4j/cache/TextCacheTest.java
@@ -1,24 +1,54 @@
 package ru.job4j.cache;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+import org.mockito.internal.util.reflection.FieldSetter;
+
+import java.lang.ref.SoftReference;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+
+import static org.junit.Assert.assertTrue;
 
 public class TextCacheTest {
+    private TxtCache txt;
+
+    @Before
+    public void setUp() {
+        txt = new TextCache();
+    }
+
+    @Test
+    public void exception() throws NoSuchFieldException {
+        FieldSetter.setField(txt, txt.getClass().getDeclaredField("directory"), null);
+        txt.getTxt("Names.txt");
+        assertTrue(true);
+    }
+
+    @Test
+    public void softRefgetEqualNull() throws NoSuchFieldException, IllegalAccessException {
+        SoftReference<?> softRef = Mockito.mock(SoftReference.class);
+        Mockito.when(softRef.get()).thenReturn(null);
+        HashMap<String, SoftReference<?>> map = new HashMap<>();
+        map.put("Names.txt", softRef);
+        Field field = txt.getClass().getSuperclass().getDeclaredField("cacheT");
+        field.setAccessible(true);
+        field.set(txt, map);
+        txt.getTxt("Names.txt");
+        assertTrue(true);
+    }
+
     @Test
     public void getTxt() {
-        TxtCache txt = new TextCache();
-
-        int n = 1000;
-        //while (n-- > 0) {
-        //    //txt.getTxt("Names.txt");
-        //    //    txt.getTxt("Address.txt");
-        //    //    txt.getTxt("log4j2.xml");
-        //    //    txt.getTxt("big.txt");
-        //    //    txt.getTxt("big2.txt");
-        //    //    try {
-        //    //        Thread.sleep(100);
-        //    //    } catch (InterruptedException e) {
-        //    //        e.printStackTrace();
-        //    //    }
-        //}
+        int n = 10;
+        while (n-- > 0) {
+            txt.getTxt("Names.txt");
+            txt.getTxt("Address.txt");
+            txt.getTxt("log4j2.xml");
+            txt.getTxt("big.txt");
+            txt.getTxt("big2.txt");
+        }
+        assertTrue(true);
     }
 }
